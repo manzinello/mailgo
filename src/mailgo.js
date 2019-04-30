4887c6360922c44f24c46918efa256af8f2e9ddf
Matteo Manzinello <matteo@manzinello.com>
2019-05-01 00:27:08 +0200

    commentata creazione dinamica html, prima versione con alert del browser

--- src/mailgo.js
+++ src/mailgo.js
@@ -5,17 +5,22 @@ console.log("mailgo is WIP");
 
 // attivo mailgo su tutti gli elementi
 mailtos.forEach(mailto => {
+  /*
   var newDiv = document.createElement("div");
   var newContent = document.createTextNode("hi!");
   newDiv.appendChild(newContent);
 
   mailto.parentNode.insertBefore(newDiv, mailto.nextSibling);
+  */
 
   mailto.addEventListener(
     "click",
     event => {
       event.preventDefault();
-      alert("clicked");
+      let r = confirm("You have clicked mailto: " + mailto.href);
+      if (r === true) {
+        location.href = mailto.href;
+      }
     },
     false
   );
