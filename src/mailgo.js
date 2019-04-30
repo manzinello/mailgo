ee4d6331c3a23eaa0015001fb6620e8a830cf958
Matteo Manzinello <matteo@manzinello.com>
2019-04-30 11:00:11 +0200

    aggiunto event listener su tutti i mailto

--- src/mailgo.js
+++ src/mailgo.js
@@ -1,1 +1,13 @@
+// ottengo tutti i mailto contenuti nella pagina
 let mailtos = document.querySelectorAll('a[href^="mailto:"]');
+
+mailtos.forEach(mailto => {
+  mailto.addEventListener(
+    "click",
+    event => {
+      event.preventDefault();
+      alert("Clicked");
+    },
+    false
+  );
+});
