b87df73c497552e8d4e4b4f5cb7b7aa1f4364e34
Matteo Manzinello <matteo@manzinello.com>
2019-04-30 11:48:17 +0200

    console.log

--- src/mailgo.js
+++ src/mailgo.js
@@ -1,6 +1,8 @@
 // ottengo tutti i mailto contenuti nella pagina
 let mailtos = document.querySelectorAll('a[href^="mailto:"]');
 
+console.log("mailgo");
+
 mailtos.forEach(mailto => {
   mailto.addEventListener(
     "click",
