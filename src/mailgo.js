71353aad88a93ed60d252cf27e6e9e716d888f53
Matteo Manzinello <matteo@manzinello.com>
2019-04-30 12:41:40 +0200

    console.log wip

--- src/mailgo.js
+++ src/mailgo.js
@@ -1,13 +1,14 @@
 // ottengo tutti i mailto contenuti nella pagina
 let mailtos = document.querySelectorAll('a[href^="mailto:"]');
 
+console.log("mailgo is WIP");
+
 // attivo mailgo su tutti gli elementi
 mailtos.forEach(mailto => {
   mailto.addEventListener(
     "click",
     event => {
       event.preventDefault();
-      // alert("Clicked");
     },
     false
   );
