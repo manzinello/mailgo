3ec3825e016dec8d28759c16d9e6210a76747c07
Matteo Manzinello <matteo@manzinello.com>
2019-04-30 12:02:10 +0200

    rimosso alert inutile

--- src/mailgo.js
+++ src/mailgo.js
@@ -1,14 +1,13 @@
 // ottengo tutti i mailto contenuti nella pagina
 let mailtos = document.querySelectorAll('a[href^="mailto:"]');
 
-console.log("mailgo");
-
+// attivo mailgo su tutti gli elementi
 mailtos.forEach(mailto => {
   mailto.addEventListener(
     "click",
     event => {
       event.preventDefault();
-      alert("Clicked");
+      // alert("Clicked");
     },
     false
   );
