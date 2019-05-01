b86c82d05b90f1652b574c4a853aa1def7dae346
Matteo Manzinello <matteo@manzinello.com>
2019-05-01 13:51:44 +0200

    aggiunto terser

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const { src, dest, parallel } = require("gulp");
+const terser = require("gulp-terser");
 
 function js() {
-  return src("src/*.js", { sourcemaps: true }).pipe(
-    dest("dist", { sourcemaps: true })
-  );
+  return src("src/*.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest("dist", { sourcemaps: true }));
 }
 
 exports.js = js;
