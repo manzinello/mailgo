88ded118ec008b4c914b4518e78015591fab6bdc
Matteo Manzinello <matteo@manzinello.com>
2019-05-03 11:57:26 +0200

    aggiunta versione min con gulp-terser

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, parallel } = require("gulp");
 const terser = require("gulp-terser");
+const rename = require("gulp-rename");
 
 function js() {
   return src("src/*.js", { sourcemaps: false }).pipe(
@@ -10,9 +11,15 @@ function js() {
 function jsmin() {
   return src("src/*.js")
     .pipe(terser())
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
     .pipe(dest("dist"));
 }
 
 exports.js = js;
 exports.jsmin = jsmin;
+
 exports.default = parallel(js, jsmin);
