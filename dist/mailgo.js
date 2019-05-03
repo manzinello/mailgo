936fbfe9a55bedf5004d0fc3470ebbeceb1aa070
Matteo Manzinello <matteo@manzinello.com>
2019-05-03 13:03:42 +0200

    gulp

--- dist/mailgo.js
+++ dist/mailgo.js
@@ -82,3 +82,8 @@ mailgos.forEach((mailgo, index) => {
     false
   );
 });
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
