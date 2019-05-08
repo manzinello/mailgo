6bb00abb6323c01a3f9eaa2e0147fbf113bb420d
Matteo Manzinello <matteo@manzinello.com>
2019-05-08 13:31:12 +0200

    aggiunto gulp-csso

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const { src, dest, parallel } = require("gulp");
 const uglify = require("gulp-uglify");
 const terser = require("gulp-terser");
 const rename = require("gulp-rename");
+const csso = require("gulp-csso");
 
 function js() {
   return src("src/*.js").pipe(dest("dist"));
@@ -19,7 +20,24 @@ function jsmin() {
     .pipe(dest("dist"));
 }
 
+function css() {
+  return src("src/*.css").pipe(dest("dist"));
+}
+
+function cssmin() {
+  return src("src/*.css")
+    .pipe(csso())
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(dest("dist"));
+}
+
 exports.js = js;
 exports.jsmin = jsmin;
+exports.css = css;
+exports.cssmin = cssmin;
 
-exports.default = parallel(js, jsmin);
+exports.default = parallel(js, jsmin, css, cssmin);
