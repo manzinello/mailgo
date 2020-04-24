9bb3188687ef5a8ab92f98b95b773e6ae3f22bb5
Matteo Manzinello <matteo@manzinello.com>
2020-04-24 12:13:24 +0200

    new webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require("path");
 
-module.exports = {
-  mode: "production",
-  entry: "./dist/mailgo.min.js",
-  output: {
-    filename: "mailgo.js",
-    path: path.resolve(__dirname),
-    library: "mailgo",
-    libraryTarget: "umd",
+module.exports = [
+  {
+    mode: "production",
+    entry: "./dist/mailgo.min.js",
+    output: {
+      filename: "mailgo.js",
+      path: path.resolve(__dirname),
+      library: "mailgo",
+      libraryTarget: "umd",
+    },
   },
-};
+  {
+    mode: "production",
+    entry: "./dist/mailgo.min.js",
+    output: {
+      filename: "./dist/mailgo.min.js",
+      path: path.resolve(__dirname),
+      library: "mailgo",
+      libraryTarget: "umd",
+    },
+  },
+];
