9c8f9283b39514ef3941383723f86cae52f64633
Matteo Manzinello <matteo@manzinello.com>
2020-07-15 12:10:40 +0200

    a start with polyfill

--- webpack/mailgo.dist.dark.ts
+++ webpack/mailgo.dist.dark.ts
@@ -1,5 +1,6 @@
 // webpack > dist/mailgo.min.js
 
+import mailgoPolyfill from "./polyfill";
 import mailgo from "../src/mailgo";
 
 // call init mailgo attached to the event DOMContentLoaded
@@ -7,5 +8,7 @@ const mailgoConfig = {
   initEvent: "DOMContentLoaded",
   dark: true,
 };
+
+mailgoPolyfill();
 
 mailgo(mailgoConfig);
--- webpack/mailgo.dist.ts
+++ webpack/mailgo.dist.ts
@@ -1,10 +1,13 @@
 // webpack > dist/mailgo.min.js
 
+import mailgoPolyfill from "./polyfill";
 import mailgo from "../src/mailgo";
 
 // call init mailgo attached to the event DOMContentLoaded
 const mailgoConfig = {
   initEvent: "DOMContentLoaded",
 };
+
+mailgoPolyfill();
 
 mailgo(mailgoConfig);
