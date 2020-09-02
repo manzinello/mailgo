5a598b347d360cdd5b474cebccac02beb9855070
Matteo Manzinello <matteo@manzinello.com>
2020-09-02 12:32:57 +0200

    re-introducing gulp to export css #62

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp");
+const { series } = require("gulp");
+
+const sass = require("gulp-sass");
+const cleanCSS = require("gulp-clean-css");
+const autoprefixer = require("autoprefixer");
+const postcss = require("gulp-postcss");
+
+const rename = require("gulp-rename");
+
+sass.compiler = require("node-sass");
+
+function css() {
+  return gulp
+    .src("./src/**/*.scss")
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest("./dist"));
+}
+
+function cssMin() {
+  return gulp
+    .src("./src/**/*.scss")
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(gulp.dest("./dist"));
+}
+
+exports.css = css;
+exports.cssMin = cssMin;
+
+exports.default = series(css, cssMin);
