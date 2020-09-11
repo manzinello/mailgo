86a6e5c09e198553e2246e3ffda69d798960cf82
Matteo Manzinello <matteo@manzinello.com>
2020-09-11 09:08:18 +0200

    new utils file

--- src/mailgo.ts
+++ src/mailgo.ts
@@ -11,7 +11,6 @@ const {
   MAILTO,
   TEL,
   CALLTO,
-  SMS,
   MAIL_TYPE,
   TEL_TYPE,
   spanHTMLTag,
@@ -19,6 +18,9 @@ const {
   pHTMLTag,
   defaultLang,
 } = require("./constants");
+
+// utils
+const { validateEmails, validateTel, copyToClipboard } = require("./utils");
 
 // const { mailgoPolyfill } = require("./polyfill");
 
@@ -1064,42 +1066,6 @@ const composedPath = (
 
     el = el.parentElement;
   }
-};
-
-// validate a single email with regex
-const validateEmail = (email: string): boolean =>
-  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    email
-  );
-
-// validate an array of emails
-const validateEmails = (arr: string[]): boolean => arr.every(validateEmail);
-
-// validate a single tel with regex
-const validateTel = (tel: string): boolean =>
-  /^[+]{0,1}[\s0-9]{0,}[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/.test(tel);
-
-// copy of a string
-const copyToClipboard = (str: string): boolean => {
-  let el: HTMLInputElement = createElement("textarea") as HTMLInputElement;
-  el.value = str;
-  el.setAttribute("readonly", "");
-  el.style.position = "absolute";
-  el.style.left = "-9999px";
-  document.body.appendChild(el);
-  let selected: Range | boolean =
-    document.getSelection().rangeCount > 0
-      ? document.getSelection().getRangeAt(0)
-      : false;
-  el.select();
-  document.execCommand("copy");
-  document.body.removeChild(el);
-  if (selected) {
-    document.getSelection().removeAllRanges();
-    document.getSelection().addRange(selected);
-    return true;
-  }
-  return false;
 };
 
 // function to check an action is enabled or not
