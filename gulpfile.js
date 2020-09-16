83dcfac4f250bee1896d87eba1346ff9db4b79ec
Matteo Manzinello <matteo@manzinello.com>
2020-09-16 14:18:06 +0200

    return to autoprefixer 9 due to a bug with gulp-postss

--- gulpfile.js
+++ gulpfile.js
@@ -13,30 +13,26 @@ const rename = require("gulp-rename");
 sass.compiler = require("node-sass");
 
 function css() {
-  return (
-    gulp
-      .src("./src/**/*.scss")
-      .pipe(sass.sync().on("error", sass.logError))
-      // .pipe(postcss([autoprefixer()]))
-      .pipe(prettier({ singleQuote: true }))
-      .pipe(gulp.dest("./dist"))
-  );
+  return gulp
+    .src("./src/**/*.scss")
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(prettier({ singleQuote: true }))
+    .pipe(gulp.dest("./dist"));
 }
 
 function cssMin() {
-  return (
-    gulp
-      .src("./src/**/*.scss")
-      .pipe(sass.sync().on("error", sass.logError))
-      // .pipe(postcss([autoprefixer()]))
-      .pipe(cleanCSS({ compatibility: "ie8" }))
-      .pipe(
-        rename({
-          suffix: ".min",
-        })
-      )
-      .pipe(gulp.dest("./dist"))
-  );
+  return gulp
+    .src("./src/**/*.scss")
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(gulp.dest("./dist"));
 }
 
 exports.css = css;
