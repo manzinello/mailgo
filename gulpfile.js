ae72de5d540e8d686a93fc385360dbfaf663ce51
Matteo Manzinello <matteo@manzinello.com>
2021-07-16 14:15:43 +0200

    ⬆️ updated gulpfile to support latest version of gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 const gulp = require("gulp");
 const { series } = require("gulp");
 
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require("sass"));
 const cleanCSS = require("gulp-clean-css");
 const autoprefixer = require("autoprefixer");
 const postcss = require("gulp-postcss");
@@ -12,8 +12,6 @@ const sourcemaps = require("gulp-sourcemaps");
 const prettier = require("gulp-prettier");
 
 const rename = require("gulp-rename");
-
-sass.compiler = require("sass");
 
 function css() {
   return gulp
